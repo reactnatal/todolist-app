fb1ddd80c8b024c46672877a27f8fc55d2ab1135
Willane Paiva <willanepaiva@hotmail.com>
2019-10-16 21:33:40 -0300

    Add material ui

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.js'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App.js';
+import 'typeface-roboto';
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'));
 
 if (module.hot) {
-  module.hot.accept()
+  module.hot.accept();
 }

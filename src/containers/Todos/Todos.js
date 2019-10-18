2032fc8943b89e94cb76ebca9158ee3a4896025a
Willane Paiva <willanepaiva@hotmail.com>
2019-10-18 06:46:24 -0300

    Add todos dynamically

--- src/containers/Todos/Todos.js
+++ src/containers/Todos/Todos.js
@@ -2,12 +2,13 @@ import React, { Fragment } from 'react';
 import TodoItem from '../../components/TodoItem/TodoItem';
 import NewButton from '../../components/NewButton/NewButton';
 
-const Todos = () => {
+const Todos = ({ todos }) => {
   return (
     <Fragment>
       <NewButton />
-      <TodoItem text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" />
-      <TodoItem text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua" />
+      {todos.map(todo => (
+        <TodoItem text={todo.text} id={todo.id} key={todo.id} />
+      ))}
     </Fragment>
   );
 };

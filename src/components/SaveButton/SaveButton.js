73b1dc6e90b1d3e28d96a936ac9308e23d38abaf
Willane Paiva <willanepaiva@hotmail.com>
2019-10-17 21:36:29 -0300

    Add save button

---
+++ src/components/SaveButton/SaveButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../Button/Button';
+import SaveIcon from '@material-ui/icons/Save';
+
+const SaveButton = () => <Button text="Save" Icon={SaveIcon} />;
+
+export default SaveButton;

64979a717b88de535b773dbbadffbb78f2cc317f
Willane Paiva <willanepaiva@hotmail.com>
2019-10-18 06:46:52 -0300

    Modify import and create fake data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import React from 'react';
 
 import Todos from './containers/Todos/Todos';
-import NewTodo from './components/NewTodo/NewTodo';
+import NewTodo from './containers/NewTodo/NewTodo';
 import ErrorMessage from './components/ErrorMessage/ErrorMessage';
 
 import './App.scss';
 
+const fakesTodos = [
+  {
+    id: 1,
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  },
+  {
+    id: 2,
+    text:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua'
+  },
+  {
+    id: 3,
+    text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit'
+  }
+];
+
 const App = () => (
   <div className="app">
     <h1>To do list</h1>
-    <Todos />
+    <Todos todos={fakesTodos} />
     <NewTodo />
     <ErrorMessage />
   </div>

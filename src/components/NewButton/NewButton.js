4feeb8cc8ee09b61be4772ecf0bbdc91c5a2d4d3
Willane Paiva <willanepaiva@hotmail.com>
2019-10-17 21:36:40 -0300

    Add new item button

---
+++ src/components/NewButton/NewButton.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Button from '../Button/Button';
+import AddIcon from '@material-ui/icons/Add';
+
+const NewButton = () => <Button text="New Note" Icon={AddIcon} />;
+
+export default NewButton;

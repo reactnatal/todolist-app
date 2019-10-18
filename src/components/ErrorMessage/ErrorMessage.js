f61783f90fef20a73a18197eff915c4a930d9a7b
Willane Paiva <willanepaiva@hotmail.com>
2019-10-17 21:46:26 -0300

    Add error message

---
+++ src/components/ErrorMessage/ErrorMessage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './ErrorMessage.scss';
+
+const ErrorMessage = () => {
+  return (
+    <div className="error-message">
+      <p>Oops..You didn’t create any notes.</p>
+    </div>
+  );
+};
+
+export default ErrorMessage;
